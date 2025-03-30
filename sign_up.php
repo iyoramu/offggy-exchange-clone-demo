b7cc540f7e227b54e3080b8bc5fe1ccfcc6b2aa9
IRUTABYOSE Yoramu <yirutabyose@gmail.com>
2025-03-30 20:28:28 +0200

    Redirect authenticated users from sign_up.php to dashboard

--- sign_up.php
+++ sign_up.php
@@ -1,4 +1,11 @@
 <?php
+session_start();
+// Redirect if already authenticated
+if (isset($_SESSION['user'])) {
+    header('Location: dashboard');
+    exit;
+}
+
 require 'petit_irutabyose_yoramu/petit_irutabyose_yoramu.php';
 
 $message = "";
